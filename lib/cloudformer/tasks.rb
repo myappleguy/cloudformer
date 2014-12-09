4b79068c2a9b2c2c04454b0654050511728e5ace
Todd Pickell <tapickell@gmail.com>
2014-12-09 14:53:18 -0600

    going back to test other way

--- lib/cloudformer/tasks.rb
+++ lib/cloudformer/tasks.rb
@@ -4,7 +4,7 @@ require 'rake/tasklib'
 
 module Cloudformer
   class Tasks < Rake::TaskLib
-    STANDARD_TASKS = [:apply, :delete, :force_delete, :status, :events, :outputs, :recreate, :stopi, :start, :validate]
+    STANDARD_TASKS = [:apply, :delete, :force_delete, :status, :events, :outputs, :recreate, :stop, :start, :validate]
     attr_reader :tasks
 
     def initialize
@@ -21,9 +21,22 @@ module Cloudformer
       end
     end
 
+    def define_tasks
+      STANDARD_TASKS.each do |task_sym|
+        desc "#{task_sym.to_s.capitalize} to all Stacks."
+        task task_sym, :stack_name do |t, args|
+          tasks_to_run(task_sym, args[:stack_name]).each do |task|
+            Rake::Task[task.name.to_sym].invoke
+          end
+        end
+      end
+    end
+
     private
 
     def tasks_by_type task_type
+      #is this getting the tasks
+      puts "TASKS: #{Rake.application.tasks}"
       Rake.application.tasks.select{ |t| t.name.start_with? task_type.to_s }
     end
 
@@ -39,15 +52,5 @@ module Cloudformer
       end
     end
 
-    def define_tasks
-      STANDARD_TASKS.each do |task_sym|
-        desc "#{task_sym.to_s.capitalize} to all Stacks."
-        task task_sym, :stack_name do |t, args|
-          tasks_to_run(task_sym, args[:stack_name]).each do |task|
-            Rake::Task[task.name.to_sym].invoke
-          end
-        end
-      end
-    end
   end
 end
